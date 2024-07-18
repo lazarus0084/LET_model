4cf4285908369ed0b8d61fcc0422ea7e3a1f86b8
rubayetjahan <armanhere56@gmail.com>
2024-07-19 02:03:19 +0530

    Calculated Ai

--- LET.cc
+++ LET.cc
@@ -7,59 +7,21 @@ void init_LET(Pave& iitpave) {
     MatrixXd Xp = iitpave.Xp; // Output coordinates !!Xd in alva file
     MatrixXd Xl = iitpave.Xl; // Transpose Load position coordinates
    //MatrixXd a = iitpave.a.transpose();   // Transpose Load radii
-    VectorXd E  = iitpave.E.transpose();   // Transpose Layer Young's moduli
-    VectorXd nu = iitpave.nu.transpose(); // Transpose Layer Poisson's ratios
-    VectorXd zi = iitpave.zi.transpose(); // // Define H as the last element of zi
+    VectorXd E  = iitpave.E;   // Transpose Layer Young's moduli
+    VectorXd nu = iitpave.nu; // Transpose Layer Poisson's ratios
+    RowVectorXd zi_temp = iitpave.zi.transpose(); // // Define H as the last element of zi
 
     
-   double H = zi(zi.size() - 2); //Thickness of last layer above subgrade
-   double Laml = zi(0)/H ; //Relative height of top layer
+   double H = zi_temp(zi_temp.size() - 2); //Thickness of last layer above subgrade
+   double Laml = zi_temp(0)/H ; //Relative height of top layer
    iitpave.Laml = Laml ; iitpave.H = H ;
    
    MatrixXd XipWip = numint_coeff(N, n, Xp, Xl, H , a);  //*filename XipWip sIntegration points and weights
 //    // Display size of XipWip
 // cout << "Size of XipWip: " << XipWip.rows() << " rows x " << XipWip.cols() << " columns." << endl;
 // cout << "Element at (4, 6): " << XipWip(9, 42) << endl;
-arb_func(n,zi,E,nu,iitpave);
-
-
-
-
-
-
-
-
-
-
-// Print matrices with clear labels and separation
-#if 0
-cout << "Xp:" << endl << Xp << endl << endl;
-cout << "Printed Xp" << endl << "-----------------------------" << endl;
-
-cout << "Xl:" << endl << Xl << endl << endl;
-cout << "Printed X1" << endl << "-----------------------------" << endl;
-
-cout << "N:" << endl << N << endl << endl;
-cout << "Printed N" << endl << "-----------------------------" << endl;
-
-cout << "n:" << endl << n << endl << endl;
-cout << "Printed n" << endl << "-----------------------------" << endl;
-
-cout << "a:" << endl << a << endl << endl;
-cout << "Printed a" << endl << "-----------------------------" << endl;
-
-cout << "nu:" << endl << nu << endl << endl;
-cout << "Printed nu" << endl << "-----------------------------" << endl;
-
-cout << "zi:" << endl << zi << endl << endl;
-cout << "Printed zi" << endl << "-----------------------------" << endl;
-
-cout << "E:" << endl << E << endl << endl;
-cout << "Printed E" << endl << "-----------------------------" << endl;
-cout << "Laml:" << endl << Laml << endl << endl;
-cout << "Printed Laml" << endl << "-----------------------------" << endl;
-#endif 
-
+arb_func(E.size(),zi_temp,E,nu,iitpave);
+cout << n;
 
 }
 
--- arb_func.cc
+++ arb_func.cc
@@ -1,7 +1,8 @@
 #include "iitpave2.h"
 
-void arb_func(int n, const VectorXd& zi, const VectorXd& E, const VectorXd& nu, const Pave& iitpave) {
-   //------------------------------------------------------------------------------
+void arb_func(int n, const VectorXd& zi_temp, const VectorXd& E, const VectorXd& nu, const Pave& iitpave) {
+    
+//------------------------------------------------------------------------------
 // DESCRIPTION:
 // This function evaluates the coefficients of integration Ai, Bi, Ci and Di
 // of each layer. These are unitless functions that embody the layered 
@@ -18,6 +19,13 @@ void arb_func(int n, const VectorXd& zi, const VectorXd& E, const VectorXd& nu, 
 // To speed up the computational time, the number of matrix inversions
 // is limited to 96, corresponding to 96 predetermined values of the
 // integration variable m in the range of 0 to 100,000, as follows:
+
+    // Print the initial zi vector
+ 
+    VectorXd zi(2);
+    zi << 260,760;
+   
+
 RowVectorXd m(96);
 m << 1e-10, 0.05, 0.10, 0.20, 0.40, 0.60, 0.80, 1.00, 1.20, 1.40, 1.60,
      1.80, 2.00, 2.20, 2.40, 2.60, 2.80, 3.00, 3.20, 3.40, 3.60, 3.80,
@@ -34,6 +42,59 @@ m << 1e-10, 0.05, 0.10, 0.20, 0.40, 0.60, 0.80, 1.00, 1.20, 1.40, 1.60,
 int Lm   = m.size();               // Lm = Length of m
 RowVectorXd ONES(Lm); ONES.setOnes();
 
+// Evaluate the parameters ABCD
+double H = zi(zi.size()-1);       // Depth to the upper boundary of the 
+                                  //inifnite (bottom) layer from the top 
+                                // surface 
+VectorXd lam(zi.size() + 2);      // Ensure `lam_temp` is sized appropriately
+lam << 0, zi, 1e20 * zi.maxCoeff(); // Include zi and the max coefficient scaled by 1e20
+lam = lam/H;
 
-} 
-  
+//VectorXd lam = lam_temp.transpose(); // Transpose to get a column vector                             // the number of layer interfaces (including
+                                // the surface of the top layer). We have 
+                                // added a infinite value (1e20*max(zi)) in 
+                                //the last place. But it will not influence 
+                                // on the outcome, since it will only be 
+                                // used in the last A and C coefficients 
+                                // equations which are remmoved from the 
+                                // system of equations before solved, since 
+                                // these A and C values are zero. These two 
+                                // equations are however organized, as it is 
+                                // easier to do in the loop instead of 
+                                // having a special case at the end.
+
+//lam = [lam0 lam1 lam2 ... lam_n-1 0] = [z0 z1 z2 ... z_n-1 0]/H
+//I.e. lami = lam(i+1)
+
+
+if (zi.size() > E.size()){
+    cout << "The length of zi is too high or the length of E is to short";
+}
+//--------------------------------------------------------------------------
+// Setup the boundary and continuity conditions
+//--------------------------------------------------------------------------
+// Define the i and j-coordinates
+RowVectorXd j = RowVectorXd::LinSpaced(n-1,0,n-2);
+RowVectorXd i = j.array() + 1;
+
+
+// Define the equations
+MatrixXd Ai = VectorXd::Ones(n-1)*ONES;
+//Equations based on the vertical stresses
+// sigmaz = [                                                    ... 
+//  1*ones(n-1,1)*ONES                                           ...   Ai
+//  exp(-(lam(i)-lam(i-1))*m)                                    ...  % Bi
+// -(1-2*nu(j)*ONES-lam(i)*m)                                    ...  % Ci
+//  (1-2*nu(j)*ONES+lam(i)*m).*exp(-(lam(i)-lam(i-1))*m)         ...  % Di
+// -exp(-(lam(i+1)-lam(i))*m)                                    ...  % A{i+1}
+// -1*ones(n-1,1)*ONES                                           ...  % B{i+1}
+//  (1-2*nu(j+1)*ONES-lam(i)*m).*exp(-(lam(i+1)-lam(i))*m)       ...  % C{i+1}
+// -(1-2*nu(j+1)*ONES+lam(i)*m)                                  ...  % D{i+1}
+// ];
+cout << Ai;
+
+ }
+
+
+
+
